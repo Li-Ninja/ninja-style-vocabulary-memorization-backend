ce3d052ec5ab2427dd7527d6355bedf6f0bbdd9d
Ninja <ninja@hjtech.tw>
2022-11-18 14:28:02 +0800

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { version }  from '../package.json';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('vocabulary-backend')
+  .setDescription('vocabulary API description')
+  .setVersion(version)
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
